2f2112dde5454fb8480e46d6cd2d88773447c0df
vaibhav kumar <vaibhav.messiah@gmail.com>
2024-12-28 19:38:32 +0530

    complete and improve the code

--- temp conv/script.js
+++ temp conv/script.js
@@ -4,6 +4,14 @@ let temp = document.getElementById("temp");
 
 function convert(){
     if(farToCel.checked){
-        temp.value = (temp.value - 32) * (5/9);
+        result=((temp.value-32)*5)/9;
+        document.querySelector(".result").innerHTML = result.toFixed(2);
+    }
+    else if(celToFar.checked){
+        result=(temp.value*9)/5+32;
+        document.querySelector(".result").innerHTML = result.toFixed(2);
+    }
+    else{
+        document.querySelector(".result").innerHTML = "Please select a conversion";
     }
 }
