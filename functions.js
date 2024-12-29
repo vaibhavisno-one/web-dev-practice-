75913ef94bcb34cb0d291a6fd8a0c98d51907d8a
vaibhav kumar <vaibhav.messiah@gmail.com>
2024-12-29 22:48:33 +0530

    complete call back  function

--- functions.js
+++ functions.js
@@ -11,12 +11,28 @@
 //     console.log('goodbye');
 // }
 
-function sum(callback,x,y){
-    let result = x+y;
-    callback(result);
-}
+// function sum(callback,x,y){
+//     let result = x+y;
+//     callback(result);
+// }
 
-function display(result){
-    console.log(result);
-}
+// function display(result){
+//     console.log(result);
+// }
+//.............................................BEST EXAMPLE OF CALLBACK FUNCTION..............................................
+// function sum(x,y){
+//     console.log(x+y);
+
+// }
+// function mul(x,y){
+//     console.log(x*y);
+// }
+
+// function calc(x,y,operator){
+    
+//     operator(x,y);
+// }
+
+// calc(1,2,sum);
+// calc(1,2,mul);
 
