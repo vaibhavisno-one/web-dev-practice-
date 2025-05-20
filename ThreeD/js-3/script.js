810a4ca00dc1207884ecc3e35673440ff63be73d
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-05-20 18:49:47 +0530

    make mesh and rotate the object

---
+++ ThreeD/js-3/script.js
@@ -0,0 +1,61 @@
+// import * as THREE from 'three';
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// camera.position.z = 5;
+
+// const canvas = document.querySelector("#draw");
+
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+// function animate() {
+//     window.requestAnimationFrame( animate );
+//     renderer.render( scene, camera );
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+// }
+// animate();
+
+
+
+//Create a Scene
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(65,window.innerWidth/window.innerHeight,0.1,100);
+scene.add(camera);
+camera.position.z = 5;
+
+
+const box = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color:"red"});
+let mesh = new THREE.Mesh(box,material);
+
+// mesh.position.x= 1;//position cube on x axis
+// mesh.position.y= 1;
+// mesh.position.z= -1;
+
+// mesh.rotation.x =1;//rotate the object on x axis
+// mesh.rotation.y =1;
+// mesh.rotation.z =1;
+
+// mesh.scale.x = 2;//resize the object on x axis
+// mesh.scale.y = 2;
+// mesh.scale.z = 2;
+
+
+
+
+scene.add(mesh);
+
+const canvas = document.querySelector("#draw");
+let renderer =  new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.render(scene,camera);
