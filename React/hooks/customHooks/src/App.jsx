f61d5b89ccd5e016b2797c8391014bbd286b44aa
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-05-28 02:08:22 +0530

    custom hooks

---
+++ React/hooks/customHooks/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css';
+import InputBox from './components/InputBox.jsx';
+
+function App() {
+
+
+  return (
+    <>
+      <InputBox/>
+    </>
+  )
+}
+
+export default App;
