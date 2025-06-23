56ae6bc0a52d8dc59ad21f3834cf1366fd45f506
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-06-23 12:00:54 +0530

    connect to mongo

---
+++ BACKEND/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+/*(First approach)
+import express from "express"
+const app = express()
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error",(error) =>{
+            console.log("ERROR:",error);
+            throw error
+        })
+
+        app.listen(process.env.port,()=>{
+            console.log(`app is listning on port ${process.env.PORT}`)
+        })
+    }
+    catch (error) {
+        console.log("ERROR", error)
+        throw error
+    }
+})()
+*/
--- React/theme-toggle/src/App.jsx
+++ React/theme-toggle/src/App.jsx
@@ -4,31 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* <themeBtn/> */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* <CardUi/> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
