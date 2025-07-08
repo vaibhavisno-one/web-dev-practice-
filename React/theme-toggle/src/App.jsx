fa19b5ab5f3cf531930df4da8e00de8b63fbd3cb
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-07-08 21:23:37 +0530

    theme-toggle

--- React/theme-toggle/src/App.jsx
+++ React/theme-toggle/src/App.jsx
@@ -1,7 +1,11 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
+
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/Theme'
+import Card from './components/Card'
+
 
 function App() {
   const [themeMode,setThemeMode] = useState("light")
@@ -28,10 +32,12 @@ function App() {
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* <themeBtn/> */}
+            <ThemeBtn/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
             {/* <CardUi/> */}
+            <Card/>
           </div>
         </div>
       </div>
