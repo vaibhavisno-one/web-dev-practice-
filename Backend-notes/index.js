b23761095bc54b905bfdf24940af71a8ff656418
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-07-23 01:01:55 +0530

    dynamic routing

--- Backend-notes/index.js
+++ Backend-notes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path =  require("path");
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')//ejs setup
 
 //route setup for / will do all those backend work for / route
@@ -12,7 +14,12 @@ app.get("/", function(req,res){
 res.render("index")// / route pae view folder ka index name ka page khulega
 })  
 
+app.get('/profile/:username', function(req,res){ //dynamic routing
+    res.send(req.params.username)
+    // res.send("chal raha hai")
+})
+
 app.listen(3000, function(){
-    console.log('chal gaya');
+    console.log('chal gaya'); 
     
 }) 
