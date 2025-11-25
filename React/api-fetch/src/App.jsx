832b0b2956372dfc7e091bfa4638047bdaccefd2
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-11-26 00:36:53 +0530

    mock github profile fetcher

--- React/api-fetch/src/App.jsx
+++ React/api-fetch/src/App.jsx
@@ -1,30 +1,41 @@
-import "./App.css"
-import { useEffect, useState } from 'react'
+import { useState } from "react";
+
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
 
-
-  const [userData,setuserData] = useState([]);
-  useEffect(()=>{
-    pokeData();
-  },[])
-
-  async function  pokeData() {
-    const url = "https://dummyjson.com/users";
+  async function fetchUser() {
+    const url = `https://api.github.com/users/${username}`;
     const response = await fetch(url);
     const data = await response.json();
-    setuserData(data.users);
+    setUser(data);
   }
 
-  console.log(userData);
-  
   return (
     <div>
-    <h1>below are user datas</h1>
-    {userData && userData.map((user)=>(
-      <h1>{user.firstName}</h1>
-    ))}
-    </div>
-  )
-}
+      <h1>GitHub Profile Presenter</h1>
 
-export default App
+      <input
+        type="text"
+        placeholder="Enter GitHub username..."
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <button onClick={fetchUser}>Fetch Profile</button>
+
+      {user && (
+        <div>
+          <img src={user.avatar_url} width={120} />
+          <h2>{user.name}</h2>
+          <p>@{user.login}</p>
+          <p>Followers: {user.followers}</p>
+          <p>Following: {user.following}</p>
+          <p>Public Repos: {user.public_repos}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
