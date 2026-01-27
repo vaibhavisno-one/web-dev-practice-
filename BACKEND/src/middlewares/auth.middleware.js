a34284b7d0f94eb409f024ee98c06278f94aadb9
Vaibhav Kumar <vaibhav.messiah@gmail.com>
2026-01-27 10:01:59 +0530

    feat: Implement JWT authentication middleware and export corrected ApiResponse class.

--- BACKEND/src/middlewares/auth.middleware.js
+++ BACKEND/src/middlewares/auth.middleware.js
@@ -1,12 +1,30 @@
 import User from "../models/user.model.js" 
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
 
 
 
-exports.protect = asynchandler(async(req,res,next)=>{
-    try{
+exports.verifyJWT = asyncHandler(async(req,res,next)=>{
+    const token = req.cookies?.accessToken || request.header("Authorization")?.replace("Bearer ", "")
 
-    }
-    catch(error){
-        throw new ApiError(401,error.message || "Invalid access token")
+    try {
+        if (!token){
+            throw new ApiError(401, "Unauthorized")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+    
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken" )
+    
+        if(!user){
+            throw new ApiError(401, "User not found")
+        }
+    
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Access Token")
     }
 })
